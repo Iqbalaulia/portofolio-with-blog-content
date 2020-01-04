0a720191751cf91bde95e49535db847b9cc89ca2
Iqbalaulia <16410100012@stikom.edu>
2020-01-05 00:52:19 +0700

    include header footer and add link blog at navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,32 +14,7 @@
 </head>
 
 <body id="top">
-    <header>
-        <div class="profile-page sidebar-collapse">
-            <nav class="navbar navbar-expand-lg fixed-top navbar-transparent bg-primary" color-on-scroll="400">
-                <div class="container">
-                    <div class="navbar-translate"><a class="navbar-brand" href="#" rel="tooltip">Muhammad Iqbal Aulia
-                            Rafi'</a>
-                        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navigation" aria-controls="navigation" aria-expanded="false"
-                            aria-label="Toggle navigation"><span class="navbar-toggler-bar bar1"></span><span
-                                class="navbar-toggler-bar bar2"></span><span
-                                class="navbar-toggler-bar bar3"></span></button>
-                    </div>
-                    <div class="collapse navbar-collapse justify-content-end" id="navigation">
-                        <ul class="navbar-nav">
-                            <li class="nav-item"><a class="nav-link smooth-scroll" href="#about">About</a></li>
-                            <li class="nav-item"><a class="nav-link smooth-scroll" href="#skill">Skills</a></li>
-                            <li class="nav-item"><a class="nav-link smooth-scroll" href="#portfolio">Portfolio</a></li>
-                            <li class="nav-item"><a class="nav-link smooth-scroll" href="#experience">Experience</a>
-                            </li>
-                            <li class="nav-item"><a class="nav-link smooth-scroll" href="#contact">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </header>
+    @include('./component/header')
     <div class="page-content">
         <div>
             <div class="profile-page">
@@ -751,18 +726,7 @@
             </div>
         </div>
     </div>
-    <footer class="footer">
-        <div class="container text-center"><a class="cc-facebook btn btn-link" href="#"><i class="fa fa-facebook fa-2x "
-                    aria-hidden="true"></i></a><a class="cc-twitter btn btn-link " href="#"><i
-                    class="fa fa-twitter fa-2x " aria-hidden="true"></i></a><a class="cc-google-plus btn btn-link"
-                href="#"><i class="fa fa-google-plus fa-2x" aria-hidden="true"></i></a><a
-                class="cc-instagram btn btn-link" href="#"><i class="fa fa-instagram fa-2x " aria-hidden="true"></i></a>
-        </div>
-        <div class="h4 title text-center">Muhammad Iqbal Aulia Rafi</div>
-        <div class="text-center text-muted">
-            <p>&copy; Iqbal Aulia. All rights reserved.</p>
-        </div>
-    </footer>
+    @include('./component/footer')
     @include('./component/js')
 </body>
 
