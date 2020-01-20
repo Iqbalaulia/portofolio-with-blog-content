e5e80b92532bde08d9c3fcd6fdc9753f1dc8b613
Iqbalaulia <16410100012@stikom.edu>
2020-01-20 23:11:06 +0700

    add access admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,27 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    // protected $redirectTo = '/admin/admin';
+    public function redirectTo(){
+        $user = Auth::user()->role;
+        switch ($user) {
+            case 'Admin':
+                    return 'admin/admin';
+                break;
+            // case 'Fundriser':
+            //         return 'fundriser/dashboard-fundriser';
+            //     break;
+            // case 'Akuntansi':
+            //         return 'akuntansi/dashboard-akuntansi';
+            //     break;
+            // case 'User':
+            //         return '/';
+            //     break;
+            default:
+                    return '/';
+            break;
+        }
+    }
 
     /**
      * Create a new controller instance.
--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ Route::name('admin.')->group(function () {
 
     Route::group([
 
-        // 'middleware'    => 'auth',
+        'middleware'    => 'auth.admin',
         'prefix'        => 'admin',
         'namespace'     => 'Admin',
 
