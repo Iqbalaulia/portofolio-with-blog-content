38f0452c96b9bfef5eb9f13de5017dcbccc5af8c
Iqbalaulia <16410100012@stikom.edu>
2020-04-30 13:20:51 +0700

    integrasi navbar

--- resources/views/admin/dashboard/component/navbar.blade.php
+++ resources/views/admin/dashboard/component/navbar.blade.php
@@ -362,7 +362,7 @@
       </div>
     </form>
     <ul class="navbar-nav navbar-right">
-      <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
+      {{-- <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown" class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
         <div class="dropdown-menu dropdown-list dropdown-menu-right">
           <div class="dropdown-header">Messages
             <div class="float-right">
@@ -486,25 +486,30 @@
             <a href="#">View All <i class="fas fa-chevron-right"></i></a>
           </div>
         </div>
-      </li>
+      </li> --}}
       <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-        <img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a>
+        <img alt="image" src="{{ asset('/admin/app-assets/images/logo/logo.png') }}" class="rounded-circle mr-1">
+        <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div></a>
         <div class="dropdown-menu dropdown-menu-right">
           <div class="dropdown-title">Logged in 5 min ago</div>
-          <a href="features-profile.html" class="dropdown-item has-icon">
+          <a href="{{route('admin.profile.index')}}" class="dropdown-item has-icon">
             <i class="far fa-user"></i> Profile
           </a>
-          <a href="features-activities.html" class="dropdown-item has-icon">
+          <a href="{{route('admin.my-blog.index')}}" class="dropdown-item has-icon">
             <i class="fas fa-bolt"></i> Activities
           </a>
-          <a href="features-settings.html" class="dropdown-item has-icon">
+          <a href="#" class="dropdown-item has-icon">
             <i class="fas fa-cog"></i> Settings
           </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item has-icon text-danger">
-            <i class="fas fa-sign-out-alt"></i> Logout
+          <a href="{{route('admin.message-admin.index')}}" class="dropdown-item has-icon">
+            <i class="fas fa-inbox"></i> Message
           </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item has-icon text-danger" class="logout" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"><i class="fas fa-sign-out-alt"></i> {{ __('Logout') }}</a>
+                <form class="dropdown-item" id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
         </div>
       </li>
     </ul>
